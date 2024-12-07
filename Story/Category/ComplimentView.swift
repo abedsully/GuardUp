56e1b296faf713b7254f538d76ed5681d89c565e
abedsully <abedsully2909@gmail.com>
2024-12-08 00:21:09 +0700

    feat: added story category view and do background adjustment

---
+++ Story/Category/ComplimentView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Guard Up
+//
+//  Created by Stefanus Albert Wilson on 12/7/24.
+//
+
+import SwiftUI
+
+struct ComplimentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
